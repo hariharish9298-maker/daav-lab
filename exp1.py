cc2ed75f08c7943b31dde483dbd04c07c5f37ac2
hariharish9298-maker <hariharish9298@gmail.com>
2026-07-29 10:54:17 +0530

    Create exp1.py

---
+++ exp1.py
@@ -0,0 +1,18 @@
+import numpy as np
+print("NumPy Version:", np. version )
+import pandas as pd
+print("Pandas Version:", pd. version )
+import matplotlib
+print("Matplotlib Version:", matplotlib. version )
+import seaborn as sns
+print("Seaborn Version:", sns. version )
+import statsmodels.api as sm
+print("Statsmodels Version:", sm. version )
+import scipy
+print("SciPy Version:", scipy. version )
+import plotly
+print("Plotly Version:", plotly. version )
+import bokeh
+print("Bokeh Version:", bokeh. version )
+import jupyterlab
+print("JupyterLab Version:", jupyterlab. version )
